9c2ae9e04844f0187db678f868787d48dc8466ce
Arlie Isaiah M Macadagdag <Arlie.Isaiah.M.Macadagdag@xe08.ey.com>
2018-06-05 05:49:11 +0800

    Add project files.

---
+++ ReSharperTest/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ReSharperTest
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var test = new List<String>();
+            Console.WriteLine(test[0]);
+
+        }
+    }
+}
